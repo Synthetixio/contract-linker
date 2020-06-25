1ab4ebf5ebbb1e2d697047309c92f64db78aa800
justin j. moses <justinjmoses@gmail.com>
2020-06-25 10:51:08 -0700

    Add BalancerSNXUSDC pool

--- build.js
+++ build.js
@@ -31,10 +31,13 @@ redirects.push('/ProtocolDAO https://etherscan.io/address/0xEb3107117FEAd7de89Cd
 redirects.push('/Oracle https://etherscan.io/address/0xaC1ED4Fabbd5204E02950D68b6FC8c446AC95362 302');
 redirects.push('/Deployer https://etherscan.io/address/0xDe910777C787903F78C89e7a0bf7F4C435cBB1Fe 302');
 redirects.push('/Fee https://etherscan.io/address/0xfeEFEEfeefEeFeefEEFEEfEeFeefEEFeeFEEFEeF 302');
+redirects.push('/ArbRewarder https://etherscan.io/address/0xA6B5E74466eDc95D0b6e65c5CBFcA0a676d893a4 302');
+
+// Third party addresses
 redirects.push('/KybersUSD https://etherscan.io/address/0x4Cb01bd05E4652CbB9F312aE604f4549D2bf2C99 302');
 redirects.push('/KyberSNX https://etherscan.io/address/0xa107dfa919c3f084a7893A260b99586981beb528 302');
 redirects.push('/KyberSNXPricing https://etherscan.io/address/0x0d8c194e877Af78Bea7D1A7b00f593AeEd7Be709 302');
-redirects.push('/ArbRewarder https://etherscan.io/address/0xA6B5E74466eDc95D0b6e65c5CBFcA0a676d893a4 302');
+redirects.push('/BalancerSNXUSDC https://etherscan.io/address/0x815F8eF4863451f4Faf34FBc860034812E7377d9 302');
 
 // LP rewards (legacy links)
 redirects.push('/Unipool https://etherscan.io/address/0x48D7f315feDcaD332F68aafa017c7C158BC54760 302');

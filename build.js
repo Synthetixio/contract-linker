eb5c5d453364a60cea56fa2bf73edcb7a090b056
justin j. moses <justin@synthetix.io>
2020-06-25 10:22:31 -0700

    Fixing LP reward addresses in prep for staking rewards (#8)

--- build.js
+++ build.js
@@ -31,14 +31,29 @@ redirects.push('/ProtocolDAO https://etherscan.io/address/0xEb3107117FEAd7de89Cd
 redirects.push('/Oracle https://etherscan.io/address/0xaC1ED4Fabbd5204E02950D68b6FC8c446AC95362 302');
 redirects.push('/Deployer https://etherscan.io/address/0xDe910777C787903F78C89e7a0bf7F4C435cBB1Fe 302');
 redirects.push('/Fee https://etherscan.io/address/0xfeEFEEfeefEeFeefEEFEEfEeFeefEEFeeFEEFEeF 302');
-redirects.push('/Unipool https://etherscan.io/address/0x48D7f315feDcaD332F68aafa017c7C158BC54760 302');
-redirects.push('/Curvepool https://etherscan.io/address/0x13B54E8271B3e45cE71D8f4fC73eA936873a34fC 302');
-redirects.push('/CurveRewards https://etherscan.io/address/0xDCB6A51eA3CA5d3Fd898Fd6564757c7aAeC3ca92 302');
-redirects.push('/iETHRewards https://etherscan.io/address/0xC746bc860781DC90BBFCD381d6A058Dc16357F8d 302');
 redirects.push('/KybersUSD https://etherscan.io/address/0x4Cb01bd05E4652CbB9F312aE604f4549D2bf2C99 302');
 redirects.push('/KyberSNX https://etherscan.io/address/0xa107dfa919c3f084a7893A260b99586981beb528 302');
 redirects.push('/KyberSNXPricing https://etherscan.io/address/0x0d8c194e877Af78Bea7D1A7b00f593AeEd7Be709 302');
 redirects.push('/ArbRewarder https://etherscan.io/address/0xA6B5E74466eDc95D0b6e65c5CBFcA0a676d893a4 302');
+
+// LP rewards (legacy links)
+redirects.push('/Unipool https://etherscan.io/address/0x48D7f315feDcaD332F68aafa017c7C158BC54760 302');
+redirects.push('/CurveRewards https://etherscan.io/address/0xDCB6A51eA3CA5d3Fd898Fd6564757c7aAeC3ca92 302');
+redirects.push('/iETHRewards https://etherscan.io/address/0xC746bc860781DC90BBFCD381d6A058Dc16357F8d 302');
+// proper links (to remove after https://github.com/Synthetixio/synthetix/pull/523 is merged and a new version of
+// the synthetix module is published)
+redirects.push(
+	'/StakingRewardssETHUniswapV1 https://etherscan.io/address/0x48D7f315feDcaD332F68aafa017c7C158BC54760 302',
+);
+redirects.push('/StakingRewardssUSDCurve https://etherscan.io/address/0xDCB6A51eA3CA5d3Fd898Fd6564757c7aAeC3ca92 302');
+redirects.push(
+	'/StakingRewardssXAUUniswapV2 https://etherscan.io/address/0x8302FE9F0C509a996573D3Cc5B0D5D51e4FDD5eC 302',
+);
+redirects.push('/StakingRewardsiETH https://etherscan.io/address/0xC746bc860781DC90BBFCD381d6A058Dc16357F8d 302');
+redirects.push(
+	'/StakingRewardsSNXBalancer https://etherscan.io/address/0xFBaEdde70732540cE2B11A8AC58Eb2dC0D69dE10 302',
+);
+redirects.push('/StakingRewardssBTCCurve https://etherscan.io/address/0x13C1542A468319688B89E323fe9A3Be3A90EBb27 302');
 
 fs.writeFileSync(redirectsFile, redirects.join('\n') + '\n');
 

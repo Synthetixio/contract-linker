a1dbbe5978c2f96b88660b063b21122831724983
AndyTCF <andy.c.f.98@gmail.com>
2021-12-06 14:36:35 +1100

    update links to include new ccc nfts

--- build.js
+++ build.js
@@ -34,9 +34,29 @@ const redirects = [];
 	} else if (network === 'mainnet') {
 		additional = [
 			{ name: 'ProtocolDAO', address: '0xEb3107117FEAd7de89Cd14D463D340A2E6917769' },
-			{ name: 'GrantsDAOV2Contract', address: '0x4cf117aaa757dad4a084025c3f23f1b67a037049' },
-			{ name: 'GrantsDAO', address: '0xeb9a82736cc030fC4A4CD4b53e9B2c67e153208d' },
-			{ name: 'AmbassadorDAO', address: '0x46abFE1C972fCa43766d6aD70E1c1Df72F4Bb4d1' },
+
+			// Grants Council
+			{ name: 'GrantsCouncilProposals', address: '0x4cf117aaa757dad4a084025c3f23f1b67a037049' },
+			{ name: 'GrantsCouncilMultisig', address: '0xeb9a82736cc030fC4A4CD4b53e9B2c67e153208d' },
+			{ name: 'GrantsCouncilNFT', address: '0x04567106db2a4661a5fda9f48719d57b372b77bf' },
+
+			// Ambassador Council
+			{ name: 'AmbassadorCouncilMultisig', address: '0x46abFE1C972fCa43766d6aD70E1c1Df72F4Bb4d1' },
+			{ name: 'AmbassadorCouncilNFT', address: '0xf74e828b79636c228683daf5078cc5cececaa37a' },
+
+			// Treasury Council
+			{ name: 'TreasuryCouncilMultisig', address: '0x99f4176ee457afedffcb1839c7ab7a030a5e4a92' },
+			{ name: 'TreasuryCouncilNFT', address: '0x0c6f93a3ccdb4de4bbab2e3de714ea48bdbaa877' },
+
+			// Spartan Council
+			{ name: 'OldCouncilDilution', address: '0x30Ba359FE295E311D24BcCb1502c7a6e89Fb2100' },
+			{ name: 'CouncilDilution', address: '0xFf4e21298E5DCE1398d6fc9857098Eae3cAF1e72' },
+			{ name: 'SpartanCouncilNFT', address: '0x023c66b7e13d30a3c46aa433fd2829763d5817c5' },
+
+			// Core Contributor Committee
+			{ name: 'CoreContributorNFT', address: '0xe5e0cc9e81beac52a0cd1cb22048597eeebc5072' },
+			{ name: 'CoreContributorCommitteeNFT', address: '0xba9984288eba7d3afbd4bddb7b5e54eda4514fcb' },
+
 			{ name: 'Oracle', address: '0xaC1ED4Fabbd5204E02950D68b6FC8c446AC95362' },
 			{ name: 'Deployer', address: '0xDe910777C787903F78C89e7a0bf7F4C435cBB1Fe' },
 			{ name: 'Fee', address: '0xfeEFEEfeefEeFeefEEFEEfEeFeefEEFeeFEEFEeF' },
@@ -50,9 +70,6 @@ const redirects = [];
 			{ name: 'KyberSNXPricing', address: '0x0d8c194e877Af78Bea7D1A7b00f593AeEd7Be709' },
 			{ name: 'BalancerSNXUSDC', address: '0x815F8eF4863451f4Faf34FBc860034812E7377d9' },
 			{ name: 'BalancerSNXREN', address: '0x330416C863f2acCE7aF9C9314B422d24c672534a' },
-			{ name: 'OldCouncilDilution', address: '0x30Ba359FE295E311D24BcCb1502c7a6e89Fb2100' },
-			{ name: 'CouncilDilution', address: '0xFf4e21298E5DCE1398d6fc9857098Eae3cAF1e72' },
-			{ name: 'SpartanCouncilNFT', address: '0x023c66b7e13d30a3c46aa433fd2829763d5817c5' },
 		];
 	}
 	for (const { name, address } of Object.values(targets).concat(additional)) {
